f4b3320e1ec9ce69baa2b2361c1cd04641015560
Yuuki TANAAMI <st13d26@gm.ibaraki-ct.ac.jp>
2016-07-07 11:42:08 +0900

    第12週課題1提出

---
+++ week12/binary_search.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a[] = {5, 7, 15, 28, 29, 31, 39, 58, 68, 70, 95};
+	int key = 39;
+	int left = 0;
+	int right = sizeof(a)/sizeof(int) - 1;
+		printf("%d ", right);
+	int center;
+
+do {
+	center = (left + right) / 2;
+	if (a[center] == key) {
+		printf("a[%d]が%dでした。", center, key); return 0;
+	} else if (a[center] < key) {
+		left = center + 1;
+	} else {
+		right = center - 1;
+		}
+	} while (left >= right);
+		printf("見つかりませんでした。");
+	return 0;
+}
